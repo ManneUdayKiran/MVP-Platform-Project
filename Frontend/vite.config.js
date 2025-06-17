e627189bf18561815ba48c2164f4a3292dc09442
MANNE UDAY KIRAN <udaykiran7615124@gmail.com>
2025-06-17 23:57:13 +0530

    fourth commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   host: '0.0.0.0',
+   port: parseInt(process.env.PORT) || 5173,
   plugins: [react()],
 })
